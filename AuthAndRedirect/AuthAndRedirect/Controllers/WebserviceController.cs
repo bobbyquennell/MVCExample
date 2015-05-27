7fc3a14f155e31f2798c864fa89e206456402179
Bobby Wang <bobbyquennell@gmail.com>
2015-05-27 16:19:59 +1000

    mvc example: collect form data from a view and redirect to other page

---
+++ AuthAndRedirect/AuthAndRedirect/Controllers/WebserviceController.cs
@@ -0,0 +1,39 @@
+﻿using AuthAndRedirect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthAndRedirect.Controllers
+{
+    public class WebserviceController : Controller
+    {
+        // GET: Webservice
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CheckForm(FormCollection form)
+        {
+            string ThirdPartyUrl = "http://www.sina.com.cn";
+            string address1 = form["emailToCheck_1"];
+            //...to check the form data here
+            
+            
+            // if check successful, redirect to other page
+            if (true)
+            {
+                //return RedirectToAction("Index","Login");// option1: log in page
+
+                return Redirect(ThirdPartyUrl);// option 2: thrid party page
+            }
+            else
+            {
+                return View("Index");
+            }
+
+        }
+    }
+}
---
+++ AuthAndRedirect/AuthAndRedirect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthAndRedirect.Startup))]
+namespace AuthAndRedirect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
